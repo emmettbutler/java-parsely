6ce77419fc0b67324268000dd4959292e8dc7199
emmett9001 <ebutler9@gmail.com>
2013-05-17 16:02:58 -0400

    more sensible typing for models

--- ParselyModel.java
+++ ParselyModel.java
@@ -4,17 +4,30 @@ import java.lang.reflect.Type;
 import com.google.gson.*;
 
 public class ParselyModel{
+    protected String url, title, section, author;
+    protected int hits, shares;
+    protected ArrayList<String> tags, thumb_urls;
+    //protected Date pub_date;
+
+    public ParselyModel(){ }
+
+    public ParselyModel(String url, String title, String section, String author,
+                int hits, int shares, ArrayList<String> tags){
+        this.url = url;
+        this.title = title;
+        this.section = section;
+        this.author = author;
+        this.hits = hits;
+        this.shares = shares;
+        this.tags = tags;
+    }
+
     public Post getAsPost(){
         return new Post(this);
     }
 }
 
 class Post extends ParselyModel{
-    private String url, title, section, author;
-    private int hits, shares;
-    private ArrayList<String> tags, thumb_urls;
-    //private Date pub_date;
-
     public Post(String url, String title, String section, String author,
                 int hits, int shares, ArrayList<String> tags){
         this.url = url;
@@ -26,13 +39,9 @@ class Post extends ParselyModel{
         this.tags = tags;
     }
 
-    public Post(){ }
-
     public Post(ParselyModel pm){
-        this(((Post)pm).getUrl(), ((Post)pm).getTitle(),
-             ((Post)pm).getSection(), ((Post)pm).getAuthor(),
-             ((Post)pm).getHits(), ((Post)pm).getShares(),
-             ((Post)pm).getTags()
+        this(pm.url, pm.title, pm.section, pm.author, pm.hits, pm.shares,
+             pm.tags
             );
     }
 
@@ -96,6 +105,7 @@ class Author extends ParselyMeta{
 class ModelDeserializer implements JsonDeserializer<ParselyModel> {
     public ParselyModel deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
         throws JsonParseException {
+
         JsonObject js = (JsonObject)json;
 
         int _shares = 0;
@@ -113,7 +123,7 @@ class ModelDeserializer implements JsonDeserializer<ParselyModel> {
             }
         }
 
-        Post pm = new Post(js.get("url").getAsString(),
+        ParselyModel pm = new ParselyModel(js.get("url").getAsString(),
                            js.get("title").getAsString(),
                            js.get("section").getAsString(),
                            js.get("author").getAsString(),
