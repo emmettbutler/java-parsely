fb97c4261a6015f94fa67a26c28c0e8bb4acd3de
emmett9001 <ebutler9@gmail.com>
2013-05-20 10:03:08 -0400

    fix up basic models for first endpoints

--- ParselyModel.java
+++ ParselyModel.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Type;
 import com.google.gson.*;
 
 public class ParselyModel{
-    protected String url, title, section, author;
+    protected String url, title, section, author, metadata, topic, name;
     protected int hits, shares;
     protected ArrayList<String> tags, thumb_urls;
     //protected Date pub_date;
@@ -12,24 +12,44 @@ public class ParselyModel{
     public ParselyModel(){ }
 
     public ParselyModel(String url, String title, String section, String author,
-                int hits, int shares, ArrayList<String> tags){
+                String metadata, String topic, String name, int hits, int shares,
+                ArrayList<String> tags){
         this.url = url;
         this.title = title;
         this.section = section;
         this.author = author;
+        this.topic = topic;
         this.hits = hits;
         this.shares = shares;
         this.tags = tags;
+        this.metadata = metadata;
+        this.name = name;
     }
 
     public Post getAsPost(){
         return new Post(this);
     }
+
+    public Author getAsAuthor(){
+        return new Author(this);
+    }
+
+    public Section getAsSection(){
+        return new Section(this);
+    }
+
+    public Topic getAsTopic(){
+        return new Topic(this);
+    }
+
+    public Referrer getAsReferrer(){
+        return new Referrer(this);
+    }
 }
 
 class Post extends ParselyModel{
     public Post(String url, String title, String section, String author,
-                int hits, int shares, ArrayList<String> tags){
+                String metadata, int hits, int shares, ArrayList<String> tags){
         this.url = url;
         this.title = title;
         this.section = section;
@@ -37,11 +57,12 @@ class Post extends ParselyModel{
         this.hits = hits;
         this.shares = shares;
         this.tags = tags;
+        this.metadata = metadata;
     }
 
     public Post(ParselyModel pm){
-        this(pm.url, pm.title, pm.section, pm.author, pm.hits, pm.shares,
-             pm.tags
+        this(pm.url, pm.title, pm.section, pm.author, pm.metadata,
+             pm.hits, pm.shares, pm.tags
             );
     }
 
@@ -72,19 +93,24 @@ class Post extends ParselyModel{
     public ArrayList<String> getTags(){
         return this.tags;
     }
+
+    public String getMetadata(){
+        return this.metadata;
+    }
 }
 
 class ParselyMeta extends ParselyModel{
     protected String name;
-    private int hits;
+    protected int hits;
 
     private enum kIdType{
         kAuthor, kTopic, kSection, kTag, kReferrer
     };
     private kIdType idType;
 
-    public ParselyMeta(String name){
+    public ParselyMeta(String name, int hits){
         this.name = name;
+        this.hits = hits;
     }
 
     public String getName(){
@@ -97,22 +123,51 @@ class ParselyMeta extends ParselyModel{
 }
 
 class Author extends ParselyMeta{
-    public Author(String author){
-        super(author);
+    public Author(ParselyModel pm){
+        super(pm.author, pm.hits);
+    }
+}
+
+class Section extends ParselyMeta{
+    public Section(ParselyModel pm){
+        super(pm.section, pm.hits);
+    }
+}
+
+class Topic extends ParselyMeta{
+    public Topic(ParselyModel pm){
+        super(pm.topic, pm.hits);
+    }
+}
+
+class Referrer extends ParselyMeta{
+    public Referrer (ParselyModel pm){
+        super(pm.name, pm.hits);
     }
 }
 
 class ModelDeserializer implements JsonDeserializer<ParselyModel> {
-    public ParselyModel deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-        throws JsonParseException {
+    public ParselyModel deserialize(JsonElement json, Type typeOfT,
+                                    JsonDeserializationContext context)
+            throws JsonParseException {
 
         JsonObject js = (JsonObject)json;
 
-        int _shares = 0;
-        JsonElement shares = js.get("shares");
-        if(shares != null){
-            _shares = shares.getAsInt();
-        }
+        String _url = js.get("url") == null ? null : js.get("url").getAsString();
+        String _title = js.get("title") == null ?
+            null : js.get("title").getAsString();
+        String _section = js.get("section") == null ?
+            null : js.get("section").getAsString();
+        String _author = js.get("author") == null ?
+            null : js.get("author").getAsString();
+        String _metadata = js.get("metadata") == null ?
+            null : js.get("metadata").getAsString();
+        String _topic = js.get("topic") == null ?
+            null : js.get("topic").getAsString();
+        String _name = js.get("name") == null ?
+            null : js.get("name").getAsString();
+        int _shares = js.get("shares") == null ? 0 : js.get("shares").getAsInt();
+        int _hits = js.get("_hits") == null ? 0 : js.get("_hits").getAsInt();
 
         JsonElement tags = js.get("tags");
         ArrayList<String> _tags = new ArrayList();
@@ -123,12 +178,8 @@ class ModelDeserializer implements JsonDeserializer<ParselyModel> {
             }
         }
 
-        ParselyModel pm = new ParselyModel(js.get("url").getAsString(),
-                           js.get("title").getAsString(),
-                           js.get("section").getAsString(),
-                           js.get("author").getAsString(),
-                           js.get("_hits").getAsInt(),
-                           _shares, _tags);
+        ParselyModel pm = new ParselyModel(_url, _title, _section, _author,
+                           _metadata, _topic, _name, _hits, _shares, _tags);
         return pm;
     }
 }
