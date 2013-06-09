f37ff9d51f0e6dd5ed7b549ef4bfdf40c5cec09d
Emmett Butler <emmett.butler321@gmail.com>
2013-06-09 15:25:27 -0400

    fix secret example conflict

--- src/main/java/com/emmettbutler/app/ExampleSecret.java
+++ src/main/java/com/emmettbutler/app/ExampleSecret.java
@@ -1,5 +1,6 @@
 // add your API key and secret key here
-public class Secret{
+// change the class and filename to Secret
+public class ExampleSecret{
     static String apikey = "mysite.com";
     static String secret = "askjdfhasdfoi87dsga89sv0a9s8dv7an9087ads7f9ask";
 }
